b3f56f3d0017b5c6ad81a641ce7b1b447b56eb5b
Michael Hassin <michaelhassin@mhassin.local>
2014-06-17 17:13:07 -0500

    adds test for quitting, adds run loop method to library with implementation that makes test pass

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,7 +1,11 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by ashleycampo on 6/17/14.
@@ -10,11 +14,13 @@ public class Library {
 
     ArrayList<Book> books = new ArrayList<Book>();
     ArrayList<String> menuOptions = new ArrayList<String>();
+    private boolean hasQuit;
 
     public Library() {
         books.add(new Book("Head First Java", "Bill", "1924"));
         books.add(new Book("Game of Thrones", "J.R.R. Martin", "1992"));
         menuOptions.add("List Books");
+        menuOptions.add("Quit");
     }
 
     public void displayWelcomeMessage(PrintStream out) {
@@ -35,5 +41,22 @@ public class Library {
 
     public void executeCommand(String command, PrintStream out) {
         out.println("Select a valid option!");
+    }
+
+
+    public void startTakingCommands(BufferedReader reader) throws IOException {
+        String input;
+        while (true){
+            input = reader.readLine();
+            if (input.equals("Quit")){
+                break;
+            }
+        }
+        hasQuit = true;
+    }
+
+
+    public boolean hasQuit() {
+        return hasQuit;
     }
 }
--- test/com/twu/biblioteca/BibliotecaTests.java
+++ test/com/twu/biblioteca/BibliotecaTests.java
@@ -5,53 +5,61 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by ashleycampo on 6/17/14.
  */
 public class BibliotecaTests {
     private Library library;
+    private PrintStream mockStream;
 
     @Before
     public void setUp() {
         library = new Library();
+        mockStream = mock(PrintStream.class);
     }
 
     @Test
     public void testWelcomeMessage() {
-        PrintStream mockStream = mock(PrintStream.class);
 
         library.displayWelcomeMessage(mockStream);
-
         verify(mockStream).println("Welcome to Biblioteca!");
 
     }
 
     @Test
     public void testListBooks() {
-        PrintStream mockStream = mock(PrintStream.class);
         library.listBooks(mockStream);
         verify(mockStream).printf("%-20s %-20s %-20s\n", "Head First Java", "Bill", "1924");
     }
 
     @Test
     public void testDisplayMenu(){
-        PrintStream mockStream = mock(PrintStream.class);
         library.displayMenu(mockStream);
         verify(mockStream).println("(0) List Books");
     }
 
     @Test
     public void checkInvalidInput() {
-        PrintStream mockStream = mock(PrintStream.class);
         library.executeCommand("bla", mockStream);
 
         verify(mockStream).println("Select a valid option!");
     }
 
+    @Test
+    public void testQuit() throws Exception {
+        BufferedReader reader = mock(BufferedReader.class);
+        when(reader.readLine()).thenReturn("Quit");
+        library.startTakingCommands(reader);
+        assertTrue(library.hasQuit());
+    }
 }
