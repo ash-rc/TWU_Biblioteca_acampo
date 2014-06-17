9ded1d6dea11f4e98897635efe2a1a133cd93395
Ashley Campo <ashleycampo@acampo.local>
2014-06-17 12:34:35 -0500

    Adds test for listing books and implements basiiiiic list books method.

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -4,4 +4,12 @@ package com.twu.biblioteca;
  * Created by ashleycampo on 6/17/14.
  */
 public class Library {
+
+    public void displayWelcomeMessage() {
+        System.out.println("Welcome to Biblioteca!");
+    }
+
+    public void listBooks() {
+        System.out.println("Head First Java");
+    }
 }
--- test/com/twu/biblioteca/BibliotecaTests.java
+++ test/com/twu/biblioteca/BibliotecaTests.java
@@ -1,7 +1,42 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /**
  * Created by ashleycampo on 6/17/14.
  */
 public class BibliotecaTests {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    Library library;
+
+    @Before
+    public void setUp() {
+        library = new Library();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUp() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testWelcomeMessage() {
+        library.displayWelcomeMessage();
+        Assert.assertEquals("Welcome to Biblioteca!\n", outContent.toString());
+
+    }
+
+    @Test
+    public void testListBooks() {
+        library.listBooks();
+
+        Assert.assertEquals("Head First Java\n", outContent.toString());
+    }
 }
